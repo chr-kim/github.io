6a7a9a3854cf2e79281ae7d77423818a8932b62a
chr-kim <chriankim@gmail.com>
2025-10-01 02:01:14 +0900

    clear codebase

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Chriankim Blog -Vade Mecum-';
-export const SITE_DESCRIPTION = 'Chriankims Technical Blog';
+export const SITE_TITLE = '-Vade Mecum-';
+export const SITE_DESCRIPTION = 'Chriankim`s Blog';
